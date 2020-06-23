16606f8879749bafdb818215ec039096a8c3e77b
Vishal Gowda <vishal.gowda@booking.com>
2020-06-23 12:00:20 +0530

    drop unused entities

--- src/main/java/com/vgowda/hasura/chinook/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vgowda.hasura.chinook.repositories;
-
-import com.vgowda.hasura.chinook.models.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-}
--- src/main/java/com/vgowda/hasura/chinook/repositories/FilmActorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vgowda.hasura.chinook.repositories;
-
-import com.vgowda.hasura.chinook.models.FilmActor;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FilmActorRepository extends CrudRepository<FilmActor, Long> {
-}
